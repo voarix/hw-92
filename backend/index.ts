1c96b2162bc05123bf79d1f219463a9d871b0f34
Kachkynov Alikhan <t8t228@gmail.com>
2025-05-20 04:43:41 +0600

    [backend] - implement login, sending and getting all messages

--- backend/index.ts
+++ backend/index.ts
@@ -6,6 +6,10 @@ import cookieParser from "cookie-parser";
 import usersRouter from "./api/routers/users";
 import mongoose from "mongoose";
 import config from "./api/config";
+import { ClientMessage, MongoMessage, ServerMessage } from "./types";
+import { randomUUID } from "node:crypto";
+import jwt from "jsonwebtoken";
+import User, { JWT_SECRET } from "./api/models/User";
 
 const app = express();
 const wsInstance = expressWs(app);
@@ -26,11 +30,109 @@ app.use("/users", usersRouter);
 const router = express.Router();
 wsInstance.applyTo(router);
 
+interface ActiveConnections {
+  ws: WebSocket;
+  userId?: string;
+}
+
+const activeConnections: { [id: string]: ActiveConnections } = {};
+const Messages: MongoMessage[] = [];
+
+router.ws("/chat", (ws, _req) => {
+  const id = randomUUID();
+  console.log("Client connected, id= " + id);
+  activeConnections[id] = { ws };
+
+  ws.send(
+    JSON.stringify({
+      type: "ALL_MESSAGES",
+      messages: Messages.slice(-30),
+    } as ServerMessage),
+  );
+
+  ws.on("message", async (messageWs) => {
+    try {
+      const decoded: ClientMessage = JSON.parse(messageWs.toString());
+      console.log("Get new message: ", decoded);
+
+      if (decoded.type === "LOGIN" && decoded.payload) {
+        const decodedToken = jwt.verify(decoded.payload, JWT_SECRET) as {
+          _id: string;
+        };
+
+        const user = await User.findById(decodedToken._id);
+        if (!user) {
+          ws.send(
+            JSON.stringify({
+              error: "Invalid token",
+            }),
+          );
+          return;
+        }
+
+        activeConnections[id].userId = String(user._id);
+        ws.send(
+          JSON.stringify({
+            type: "ALL_MESSAGES",
+            messages: Messages.slice(-30),
+          }),
+        );
+      } else if (decoded.type === "SEND_MESSAGE" && decoded.payload) {
+        const userId = activeConnections[id].userId;
+        if (!userId) {
+          ws.send(
+            JSON.stringify({
+              error: "You are not logged in",
+            }),
+          );
+          return;
+        }
+
+        const user = await User.findById(userId);
+        if (!user) {
+          ws.send(
+            JSON.stringify({
+              error: "User not found",
+            }),
+          );
+          return;
+        }
+
+        const newMessage: MongoMessage = {
+          _id: randomUUID(),
+          user: {
+            _id: String(user._id),
+            username: user.username,
+          },
+          text: decoded.payload,
+          createdAt: new Date(),
+        };
+        Messages.push(newMessage);
+
+        Object.values(activeConnections).forEach((connectionWs) => {
+          connectionWs.ws.send(JSON.stringify(newMessage));
+        });
+      }
+    } catch (error) {
+      ws.send(
+        JSON.stringify({
+          error: "Invalid message format",
+        }),
+      );
+    }
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected, id= " + id);
+    delete activeConnections[id];
+  });
+});
+
+app.use(router);
 const run = async () => {
   await mongoose.connect(config.db);
-
   app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
+    console.log(`Server started on localhost:${port}`);
   });
 
   process.on("exit", () => {
--- backend/types.d.ts
+++ backend/types.d.ts
@@ -4,3 +4,23 @@ export interface UserFields {
   token: string;
   __confirmPassword: string;
 }
+
+export interface MongoMessage {
+  _id: string;
+  user: {
+    _id: string;
+    username: string;
+  };
+  text: string;
+  createdAt: Date;
+}
+
+export interface ClientMessage {
+  type: "LOGIN" | "SEND_MESSAGE" | "LOGOUT";
+  payload?: string;
+}
+
+interface ServerMessage {
+  type: "NEW_MESSAGE" | "ALL_MESSAGES";
+  payload?: MongoMessage[];
+}
